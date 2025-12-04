a6cd89991c5ff8b461bc0bcae3a9f48a12329886
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-12-04 13:54:38 +1100

    Finished Day 3

--- Challenge3/main.py
+++ Challenge3/main.py
@@ -1,12 +1,37 @@
-def part_one(line: str) -> int:
-    max = int(line[0] + line[1])
+import numpy as np
 
-    for i in range(len(line) - 1):
-        for j in range(i + 1, len(line)):
-            num = int(line[i] + line[j])
-            if num > max:
-                max = num
-    return max
+
+# Solves both part 1 and 2
+def list_to_number(lst: list) -> int:
+    num = 0
+    for i in range(len(lst)):
+        num += lst[i]
+        if i != len(lst) - 1:
+            num *= 10
+    return num
+
+
+def get_max_number(line: str, length: int, answer_lst=[]) -> int:
+    line_list = [int(i) for i in list(line)]
+
+    if len(line_list) == length:
+        answer_lst += line_list
+        return list_to_number(answer_lst)
+
+    if length != 1:
+        sub_list = line_list[: -(length - 1)]
+    else:
+        sub_list = line_list
+
+    index_max = int(np.argmax(sub_list))
+    answer_lst.append(sub_list[index_max])
+    line = line[index_max + 1 :]
+    length -= 1
+
+    if length == 0:
+        return list_to_number(answer_lst)
+    else:
+        return get_max_number(line, length, answer_lst)
 
 
 if __name__ == "__main__":
@@ -15,8 +40,12 @@ if __name__ == "__main__":
     input_lst = input_file.split("\n")
 
     solution = 0
-
     for line in input_lst:
-        solution += part_one(line)
-
+        solution += get_max_number(line, length=2, answer_lst=[])
     print(f"Part One Solution: {solution}")
+
+    solution = 0
+    for line in input_lst:
+        solution += get_max_number(line, length=12, answer_lst=[])
+
+    print(f"Part Two Solution: {solution}")
